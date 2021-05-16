5baf2aabb4c76ef139a824d2df1a99a83c51f7a5
Balaji Seetharaman <balaji.seetharaman@gmail.com>
2021-05-16 19:19:17 +0530

    Updated data wrangling method

--- Python/py_first_principles/AZ_Excel_Read.py
+++ Python/py_first_principles/AZ_Excel_Read.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import os as os
-from AZ_Module import test_mod as k
+from AZ_Module import test_mod
 class file_handle:
     def __init__(self,name,path):
         self.name=name
@@ -21,16 +21,23 @@ class file_handle:
 
 ## Data Wrangling
     def data_stats(self):
-        self.df['ServiceLine']=self.df['Service'].str.split('-').str[0]
-        self.df['ServiceArea']=self.df['Service'].str.split('-').str[1]
-        self.df.drop(['Service'],axis=1,inplace=True)
-        self.df['Cert_year'] = pd.DatetimeIndex(self.df['Cert_Date']).year
-        self.df=self.df[['Vendor','ServiceLine','ServiceArea','Geography','CIC_Geo','Certification Name','Cert_Date','Cert_year','Expiry Date','Active','Target']]
-        print(self.df[self.df.Vendor=='IBM'])  ## Filter Dataframe for IBM
+        df=self.df.copy()  # copying a dataframe and retaining the original df undisturbed
+        df['ServiceLine']=df['Service'].str.split('-').str[0]
+        df['ServiceArea']=df['Service'].str.split('-').str[1]
+        df.drop(['Service'],axis=1,inplace=True)
+        df['Cert_year'] = pd.DatetimeIndex(df['Cert_Date']).year
+        df=df[['Vendor','ServiceLine','ServiceArea','Geography','CIC_Geo','Certification Name','Cert_Date','Cert_year','Expiry Date','Active','Target']]
+        df=df[(df.Vendor!= "Error")]  ## Filter specific row
+        df=df.dropna()  # Dropping Na row
+        df.shape
+        df.info()
+        #Renaming set of columns
+        df.rename(columns={'Certification Name':'Certification_Name','Expiry Date':'Expiry_Date'},inplace=True)
+        print(df.columns)
 
 ## Object Creation
 a=file_handle('TEMPLATE.xlsx',"/Users/Balaji/Sandbox/Portfolio/DataScience/Python/py_first_principles/")
 a.file_check()
 a.file_read()
 a.data_stats()
-k=test_mod('end')  # calling a method in another python file
+#k=test_mod('end')  # calling a method in another python file
