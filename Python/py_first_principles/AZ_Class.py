681f71dd24fe5f347a0f053569e8239e892ffd57
Balaji Seetharaman <balaji.seetharaman@gmail.com>
2021-05-16 18:19:01 +0530

    Created extract method

--- Python/py_first_principles/AZ_Class.py
+++ Python/py_first_principles/AZ_Class.py
@@ -19,14 +19,14 @@ print(id(b))
 
 
 # Result of the above code
-calling init method with id value as below
-4474940752  # Object a
-calling object a with id value as below
-4474940752  # Object a
-calling init method with id value as below
-4474940880  # Object b
-calling object b with id value as below
-4474940880   # Object b
+#calling init method with id value as below
+#4474940752  # Object a
+#calling object a with id value as below
+#4474940752  # Object a
+#calling init method with id value as below
+#4474940880  # Object b
+#calling object b with id value as below
+#4474940880   # Object b
 
 
 ####### Assigning variable in init
@@ -66,25 +66,42 @@ class Apple:
     manufacturer = 'Apple Inc'
     contact_website = 'www.apple.com/contact'
     name = 'Apple'
+    year_of_manufac=2019
 
     def contact_details(self):
         print('Contact us at ', self.contact_website)
         print('apple class id :',id(self))
 
 
-class MacBook(Apple):    # child class inherits the parent class
+class MacBook(Apple): # child class inherits the parent class
+    "This is a Macbook Class - docstring text "
     def __init__(self):
         self.year_of_manufacture = 2018
         print('macbook id init method:',id(self))
 
 
     def manufacture_details(self):
+        self.created=20201210
         print('macbook id manufacture details:',id(self))
         print('This MacBook was manufactured in {0}, by {1}.'
               .format(self.year_of_manufacture, self.manufacturer))  # manufactured is through the parent class
 
+    def extract_methods(self):
+        method_list = [num for num in list(dir(MacBook)) if num.startswith('__') is True]
+        print(method_list)
+
 brand=Apple()
 print('brand object id',id(brand))
+print(type(brand))
 brand.contact_details()
 macbook = MacBook()
 macbook.manufacture_details()
+macbook.extract_methods()
+print(macbook.__doc__)   # printing docstring
+print(macbook.__class__)
+print(macbook.__dict__)
+print(macbook.__format__)
+print(macbook.__getattribute__)
+print(macbook.__delattr__)
+print(macbook.__dir__)
+print(macbook.__hash__)
